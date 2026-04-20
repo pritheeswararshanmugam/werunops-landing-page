eaf9026e0b0f8b922e2671cc58e7a9269a67ec38
Pritheeswarar <spritheeswarar303@gmail.com>
2026-04-20 12:27:41 +0530

    re-deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/werunops-landing-page/' : '/',
-  plugins: [react()],
-}));
+export default defineConfig(({ mode }) => {
+  const isNetlify = process.env.NETLIFY === 'true';
+
+  return {
+    base: mode === 'production' ? (isNetlify ? '/' : '/werunops-landing-page/') : '/',
+    plugins: [react()],
+  };
+});
